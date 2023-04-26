c4932302fcb1c9a3d32f265d9047727d14b32e95
Henriettaiyare <iyarehenrietta@gmail.com>
2023-04-25 17:07:13 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,20 @@
+1 #include "main.h"
+  2 /**
+  3  * main - print _putchar
+  4  * is written by mbah
+  5  * Return: 0
+  6  */
+  7 
+  8 int main(void)
+  9 {
+ 10 putchar('');
+ 11 _putchar('p');
+ 12 _putchar('u');
+ 13 _putchar('t');
+ 14 _putchar('c');
+ 15 _putchar('h');
+ 16 _putchar('a');
+ 17 _putchar('r');
+ 18 _putchar('\n');
+ 19 return (0);
+ 20 }
